2dfb9516d73094f7f33b4ea83eb8879df5856646
Alexandre Almeida <alexandre.almeida@vr2work.com>
2023-05-19 15:49:29 +0100

    Updates

---
+++ _app/immutable/chunks/paths.cb31acc1.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_15g2dq1)==null?void 0:s.base)??"/quiz-app";var a;const t=((a=globalThis.__sveltekit_15g2dq1)==null?void 0:a.assets)??e;export{t as a,e as b};
